f0b24ea84c803c5e7327ccce88270798a62b68a1
Shreeti-Bajracharya <shreetibajracharya73@gmail.com>
2025-11-10 13:05:49 +0545

    Backend Practice and tested the login signup function along eith add delete list and single products on the thunder client

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,30 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js"
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
 
 // app config
-const app=express()
-const port=process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
 
 // middlewares
 app.use(express.json())
 app.use(cors())
 
 // api end points
-app.get('/', (req, res)=>{
-    res.send("API Working")
-})
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
 
-app.listen(port, ()=>console.log('Server started on PORT : '+ port))
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+
+
+app.listen(port, () => console.log("Server started on PORT : " + port));
