cbe77d9074e11786c907a3d44cdde60969e80f30
Shreeti-Bajracharya <shreetibajracharya73@gmail.com>
2025-11-10 13:05:49 +0545

    Backend Practice and tested the login signup function along eith add delete list and single products on the thunder client

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB=async()=>{
 
+    mongoose.connection.on('connected', ()=>{
+        console.log("DB Connected");
+    })
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
 }
 export default connectDB;
 // 5.29.40
