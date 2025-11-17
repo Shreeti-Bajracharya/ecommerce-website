5e87b4aa6755669faea4fa538dc896da146fee51
Shreeti-Bajracharya <shreetibajracharya73@gmail.com>
2025-11-17 14:34:08 +0545

    Amost done but errors present

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { backendUrl } from "../App";
+import { toast } from "react-toastify";
+import { assets } from "../assets/assets";
 
-const Orders = () => {
-  return (
+const Orders = ({ token }) => {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    if (!token) {
+      return null;
+    }
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/order/list",
+        {},
+        { headers: { token } }
+      );
+      if (response.data.success) {
+        setOrders(response.data.orders);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+  useEffect(() => {
+    fetchAllOrders();
+  }, [token]);
+
+  return (<div>
+    <h3>Order Page</h3>
     <div>
-      
-    </div>
-  )
-}
+      {
+        orders.map((order , index)=>(
+          <div key={index}>
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              {order.items.map((item, index)=>{
+                if(index===order.items.length){
+                  return <p key={index}>{item.name} * {item.quantity} <span>{item.size}</span></p>
 
-export default Orders
+                }
+              })}
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  </div>);
+};
+
+export default Orders;
