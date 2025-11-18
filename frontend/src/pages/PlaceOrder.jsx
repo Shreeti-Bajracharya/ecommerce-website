c050dee133e7fdd22d7349e0f3583ce8f37c289d
Shreeti-Bajracharya <shreetibajracharya73@gmail.com>
2025-11-18 13:54:50 +0545

    Done

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -64,16 +64,32 @@ const PlaceOrder = () => {
 
       switch (method) {
         case "cod":
-          const response = await axios.post(backendUrl + '/api/order/place', orderData, {headers:{token}})
+          const response = await axios.post(
+            backendUrl + "/api/order/place",
+            orderData,
+            { headers: { token } }
+          );
           // console.log(response.data.success);
-          if(response.data.success){
+          if (response.data.success) {
             setCartItems({});
-            navigate('/orders')
-          }else{
+            navigate("/orders");
+          } else {
             toast.error(response.data.message);
           }
-        break;
-
+          break;
+        // case "stripe":
+        //   const responseStripe = await axios.post(
+        //     backendUrl + "/api/order/stripe",
+        //     orderData,
+        //     { headers: { token } }
+        //   );
+        //   if (responseStripe.data.success) {
+        //     const { session_url } = responseStripe.data;
+        //     window.location.replace(session_url);
+        //   } else {
+        //     toast.error(responseStripe.data.message);
+        //   }
+        //   break;
         default:
           break;
       }
